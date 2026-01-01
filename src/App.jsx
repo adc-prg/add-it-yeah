8c374a1a2ce86b39548552c061bf523c4c93af59
adc-prg <adeetya913@gmail.com>
2026-01-01 21:35:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,10 +37,6 @@ const cvData = {
         interests: "I am interested in abstract algebra and topology, especially commutative algebra and algebraic geometry, and enjoy working on anything with rich algebraic structure.",
         ambition: "Apart from academics, I play football, cards, video games and board games. I am recently exploring alot of music, art and literature."
         },
-    link: {
-            url: "https://www.iiserb.ac.in/",
-            text: "IISER Bhopal"
-        },
     research: [{
         title: "Exploring Integrality and Genus of some specific Cayley Graphs",
         guide: "Guide : Prof. Ashish Upadhyay",
